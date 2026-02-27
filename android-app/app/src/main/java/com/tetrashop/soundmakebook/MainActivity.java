88a3b93c0415db4d1f3537c793efb4083aacd1f5
نام شما <email@example.com>
2026-02-27 12:35:19 +0330

    fix: add missing import for WebChromeClient

--- android-app/app/src/main/java/com/tetrashop/soundmakebook/MainActivity.java
+++ android-app/app/src/main/java/com/tetrashop/soundmakebook/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.webkit.WebViewAssetLoader;
 import androidx.webkit.WebViewAssetLoader.AssetsPathHandler;
+import android.webkit.WebChromeClient;
 
 import java.io.BufferedReader;
 import java.io.File;
